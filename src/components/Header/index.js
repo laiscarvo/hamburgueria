a2bc86bb687d189daf919f993528103833fba1a5
Lais Carvalho de Oliveira <99590932+laiscarvo@users.noreply.github.com>
2022-08-08 12:41:17 -0300

    fix: botao pesquisar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,42 @@
-import React from 'react'
+import React from "react";
 
-import { useState } from 'react'
-import { Container } from '../../styles/global'
-import { ThemeInput } from '../../styles/input'
-import { ThemeButton } from '../../styles/button'
-import { StyledHeader } from './style'
+import { useState } from "react";
+import { ThemeInput } from "../../styles/input";
+import { ThemeButton } from "../../styles/button";
+import { StyledHeader } from "./style";
 
-import Logo from '../../assets/MaskGroup.svg'
-
-
+import Logo from "../../assets/MaskGroup.svg";
 
 const Header = ({ showProducts }) => {
+  const [searchInput, setSearchInput] = useState("");
 
-    const [ searchInput, setSearchInput] = useState("")
+  return (
+    <StyledHeader>
+      <div className="header">
+        <img src={Logo} alt="Logo" />
+        <div className="search">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              showProducts(searchInput);
+            }}
+          >
+            <ThemeInput
+              type="text"
+              InputSize="lg"
+              InputStyle="solid"
+              placeholder="Digite sua pesquisa"
+              value={searchInput}
+              onChange={(event) => setSearchInput(event.target.value)}
+            ></ThemeInput>
+            <ThemeButton type="submit" buttonSize="md" buttonStyle="solid">
+              Pesquisar
+            </ThemeButton>
+          </form>
+        </div>
+      </div>
+    </StyledHeader>
+  );
+};
 
-    return(
-        <StyledHeader>
-            <div className='header'>
-                <img src={Logo} alt="Logo" />
-                <div className='search'>
-                    <form onSubmit={(event) => {
-                        event.preventDefault()
-                        showProducts(searchInput)
-                    }}>
-                        <ThemeInput 
-                            type="text"
-                            InputSize="lg"
-                            InputStyle="solid"
-                            placeholder="Digite sua pesquisa"
-                            value={searchInput}
-                            onChange= {(event) => setSearchInput(event.target.value)}>    
-                        </ThemeInput>
-                        <ThemeButton type="submit" buttonSize="md" buttonStyle="solid">Teste</ThemeButton>
-                    </form>
-                </div>
-            </div>  
-        </StyledHeader>   
-    )
-}
-
-export default Header
+export default Header;
